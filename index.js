bdbc430344b52c498bcb5f009a92b849cf3d8c5d
Paris-Jane <parisjan@byu.edu>
2025-12-01 10:02:30 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+// Paris Ward, Lucas Moraes, Joshua Ethington, Parker Sandstrom
+// This code will allow users of a non profit to manage visitor information, user information, events, milestones, and donations
